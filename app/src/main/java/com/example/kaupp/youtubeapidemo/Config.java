694c79c3cb4dea0e3d16021b1acf695d1f6d5e56
K2418 <K2418@student.jamk.fi>
2017-10-07 13:34:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kaupp/youtubeapidemo/Config.java
@@ -0,0 +1,12 @@
+package com.example.kaupp.youtubeapidemo;
+
+/**
+ * Created by kaupp on 7.10.2017.
+ */
+
+public final class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBBuKKJ-AWmbHwKSaVGuZArlKddCBRYCBc";
+}
